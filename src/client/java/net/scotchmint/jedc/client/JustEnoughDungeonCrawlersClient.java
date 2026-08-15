253148ebcf3a5a7372c6f7a981944d5d64e02d40
fareskingtube <fareskingtube@gmail.com>
2026-08-15 12:51:49 +0300

    init

---
+++ src/client/java/net/scotchmint/jedc/client/JustEnoughDungeonCrawlersClient.java
@@ -0,0 +1,10 @@
+package net.scotchmint.jedc.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JustEnoughDungeonCrawlersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
