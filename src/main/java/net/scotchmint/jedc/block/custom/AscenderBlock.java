0c3299260639b1f6ab23e6cae3f1c000e8309716
fareskingtube <fareskingtube@gmail.com>
2026-08-15 19:11:46 +0300

    Add currentLevel and get to next level on key use

--- src/main/java/net/scotchmint/jedc/block/custom/AscenderBlock.java
+++ src/main/java/net/scotchmint/jedc/block/custom/AscenderBlock.java
@@ -6,9 +6,11 @@ import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
@@ -29,15 +31,31 @@ public class AscenderBlock extends BlockWithEntity {
 
     @Override
     protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (stack.getItem() == ModItems.DIMENSIONAL_KEY) {
 
-        if (world != null && !world.isClient() && world.getServer() != null) {
-            JustEnoughDungeonCrawlers.LOGGER.info("test");
-            RegistryKey<World> DimKey = DimensionalManager.getOrCreateVoidTier(world.getServer(), (ServerPlayerEntity) player, 1);
+        if (stack.getItem() == ModItems.DIMENSIONAL_KEY) {
+            BlockEntity be = world.getBlockEntity(pos);
+            if (be instanceof AscenderBlockEntity ascender) {
+                if (world instanceof ServerWorld serverWorld) {
+                    int currentLevel = ascender.getCurrentTier(serverWorld);
+                    PlayerInventory inventory = player.getInventory();
+                    JustEnoughDungeonCrawlers.LOGGER.info("test");
+                    RegistryKey<World> DimKey = DimensionalManager.getOrCreateVoidTier(world.getServer(), (ServerPlayerEntity) player, currentLevel + 1);
+                    for (int i = 0; i < inventory.size(); i++) {
+                        ItemStack iStack = inventory.getStack(i);
+                        
+                        if (!iStack.isEmpty() && iStack.isOf(ModItems.DIMENSIONAL_KEY)) {
+                            inventory.setStack(i, ItemStack.EMPTY);
+                        }
+
+                        player.currentScreenHandler.sendContentUpdates();
+                    }
+                    return ItemActionResult.SUCCESS;
+                }
+            }
+
+            return ItemActionResult.FAIL;
         }
-        return ItemActionResult.SUCCESS;
-        }
-        return  ItemActionResult.FAIL;
+        return ItemActionResult.FAIL;
     }
 
     @Override
--- src/main/java/net/scotchmint/jedc/block/entity/custom/AscenderBlockEntity.java
+++ src/main/java/net/scotchmint/jedc/block/entity/custom/AscenderBlockEntity.java
@@ -1,13 +1,55 @@
 package net.scotchmint.jedc.block.entity.custom;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.scotchmint.jedc.JustEnoughDungeonCrawlers;
 import net.scotchmint.jedc.block.entity.ModBlockEntities;
 
 public class AscenderBlockEntity extends BlockEntity {
-    public AscenderBlockEntity( BlockPos pos, BlockState state) {
-        super(ModBlockEntities.REVIVAL_ALTAR_BE ,pos, state);
+//    private int currentLevel = 1;
+
+
+    public int getCurrentTier(ServerWorld world) {
+        String path = world.getRegistryKey().getValue().getPath(); // e.g. "void_tier_3"
+        if (path.startsWith("dungeon_tier_")) {
+            try {
+                return Integer.parseInt(path.substring("dungeon_tier_".length()));
+            } catch (NumberFormatException e) {
+                JustEnoughDungeonCrawlers.LOGGER.error("Malformed void tier dimension id: {}", path);
+            }
+        }
+        return 0; // overworld or unrecognized dimension = base tier
+    }
+
+
+    public void setCurrentLevel(int currentLevel) {
+//        this.currentLevel = currentLevel;
+        markDirty();
+        if (world != null && !world.isClient) {
+            world.updateListeners(pos, getCachedState(), getCachedState(), Block.NOTIFY_ALL);
+        }
+    }
+
+    public AscenderBlockEntity(BlockPos pos, BlockState state) {
+        super(ModBlockEntities.REVIVAL_ALTAR_BE, pos, state);
+    }
+
+    @Override
+    protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+        super.writeNbt(nbt, registryLookup);
+
+//        nbt.putInt("currentLevel", this.currentLevel);
+    }
+
+    @Override
+    protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+        super.readNbt(nbt, registryLookup);
+
+//        this.currentLevel = nbt.getInt("currentLevel");
     }
 }
